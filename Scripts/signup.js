d5f102e10e4b16539ccc90dbc2a22eebc9551dac
Raman Kumar <KUMARR8Q@novartis.net>
2024-05-08 01:55:45 +0530

    added signup.html

--- Scripts/signup.js
+++ Scripts/signup.js
@@ -2,7 +2,6 @@
     var intervalHandle = setInterval(
         function () {
             if (window.pageReady) {
-
                 function GetParameterValues(param) {
                     var url = document.referrer.slice(window.location.href.indexOf('?') + 1).split('&');
                     for (var i = 0; i < url.length; i++) {
@@ -15,56 +14,23 @@
                     return null;
                 };
 
+                function loadFields() {
+                    var fieldInfo = $.parseJSON("#FieldInfo").val();
+                    fieldInfo.Fields_Info.forEach(function (UXField) {
+                        console.log(UXField.Id);
+                    });
+                }
                 var continuteButton = document.getElementById('continue');
                 if (continuteButton && $("#continue").is(':visible')) {
+
+                    loadFields();
 
                     $("#continue").click(function (e) {
                         debugger;
                         e.preventDefault();
                         e.stopPropagation();
                     });
-
-                    var Idvalues = $("#form_ids").val();
-                    var formIds = Idvalues.split(",");
-                    for (var index in formIds) {
-                        var fieldClass = "." + formIds[index] + "_li";
-                        $(fieldClass).show();
-                    }
-
-                    if ($("#customCancel") && $("#customCancel").is(':visible')) {
-                        $("#customCancel").remove();
-                    }
-                    if ($("#customContinue") && $("#customContinue").is(':visible')) {
-                        $("#customContinue").remove();
-                    }
-
-                    $("#continue").after("<button id='customCancel'>Cancel</button>");
-
-                    $("#continue").after("<button id='customContinue'>Continue</button>");
-                    $("#continue").hide();
-                    $("#btnConsent").click(function () {
-
-                        var portalURL = $("#passwordResetPortalUserURl").val();
-
-                        if (portalURL != null && portalURL != undefined)
-                            window.location.replace(portalURL);
-                    });
-
-                    $("#customContinue").click(function () {
-                        console.log("customCancel clicked");
-                        if ($("#speciality_required") && $("#speciality_required").is(':visible')) {
-                            $("#speciality_value").val($("#speciality_required").val());
-                        }
-                        else {
-                            $("#speciality_required").get(0).selectedIndex = 1;
-                            $("#speciality_value").val($("#speciality").val());
-                        }
-                        $("#continue").click();
-
-                    });
-                    $("#customCancel").click(function () {
-                        debugger;
-
+                    $("#Cancel").click(function () {
                         var returnUrl = GetParameterValues('return_url'); //Encoded value FE URL
                         if (returnUrl == null)
                             returnUrl = "";
